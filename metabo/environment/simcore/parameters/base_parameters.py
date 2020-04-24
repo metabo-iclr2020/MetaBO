61e05b0b11a18ad421341fa2e179c763433ffa17
vml2si <michael.volpp@de.bosch.com>
2020-04-24 15:25:26 +0200

    Code moved to https://github.com/boschresearch/MetaBO

--- metabo/environment/simcore/parameters/base_parameters.py
+++
@@ -1,6 +0,0 @@
-class Parameter:
-    """The Parameter class defines objects containing parameters which describe
-    an environment or a simulation configuration
-    """
-    def __init__(self):
-        pass
